a89b22274f4227a4e454c77e14fd558f62fe64f1
ctscofield <ctscofield@gmail.com>
2021-06-03 17:17:09 -0600

    index

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("hey there UwU")
+require("dotenv").config()
+const express = require("express")
+const server = express()
+console.log(process.env.HOSTNAME);
+
+if (process.env.NODE_ENV === "production") {
+    console.log("this means this code is deployed!")
+}
+
+const PORT = process.env.PORT || 5000
+
+console.log("port is > ", PORT)
+
+server.get("/api", (req, res) => {
+    res.json({ message: `${process.env.COHORT} Rocks`})
+})
+
+server.listen(PORT, () => {
+    console.log(`Listening on ${PORT}`)
+})
